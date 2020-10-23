87aa1b757c8286443490102181cc063de0707174
Luis Reyes <luis.reyes@rgluis.com>
2020-10-23 00:12:04 -0500

    Docs added

---
+++ docs/precache-manifest.204ffa015b3e2599d350e774e4e02424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660c588db4385bf8ba069f653701b4e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c3b6bb299199be6a2e",
+    "url": "/static/css/main.67e58c6c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c3b6bb299199be6a2e",
+    "url": "/static/js/main.ea44c019.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
